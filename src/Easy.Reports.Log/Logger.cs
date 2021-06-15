9194b026011d35a2d7747e2ceeb2c5bf42123e68
Diego Lacerda <diego.lacerda.alves@gmail.com>
2021-06-14 23:48:23 -0300

    ajustes

---
+++ src/Easy.Reports.Log/Logger.cs
@@ -0,0 +1,26 @@
+﻿using Easy.Reports.Domain.Interfaces;
+using System;
+using System.Text;
+
+namespace Easy.Reports.Log
+{
+    public class Logger : ILogger
+    {
+        private StringBuilder sb;
+        public void Error(Exception exception, string message)
+        {
+            sb = new StringBuilder();
+            sb.AppendLine("Level: [Error]");
+            sb.AppendLine($"Message: [{message}]");
+            sb.AppendLine($"Exception: [{exception}]");
+            Console.WriteLine(sb.ToString());
+        }
+        public void Info(string message)
+        {
+            sb = new StringBuilder();
+            sb.AppendLine("Level: [Information]");
+            sb.AppendLine($"Message: [{message}]");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
