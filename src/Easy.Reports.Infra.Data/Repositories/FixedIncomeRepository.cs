8694ae151e301eb0e41b6b5e06c4a69251f0ffc9
Diego Lacerda <diego.lacerda.alves@gmail.com>
2021-06-16 01:06:31 -0300

    removido namespace não utilizado

--- src/Easy.Reports.Infra.Data/Repositories/FixedIncomeRepository.cs
+++ src/Easy.Reports.Infra.Data/Repositories/FixedIncomeRepository.cs
@@ -2,7 +2,6 @@
 using Easy.Reports.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Easy.Reports.Infra.Data.Repositories
